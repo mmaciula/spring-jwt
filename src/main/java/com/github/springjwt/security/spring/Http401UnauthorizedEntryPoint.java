dfe889b570eac6c5c47d24dcfde1599c4e0c8139
mariusz <mariusz.maciula@gmail.com>
2018-10-11 00:45:13 +0200

    PasswordEncoder & 401 error classes

---
+++ src/main/java/com/github/springjwt/security/spring/Http401UnauthorizedEntryPoint.java
@@ -0,0 +1,22 @@
+package com.github.springjwt.security.spring;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Returns 401 error code to the Client (Unauthorized)
+ */
+
+@Component
+public class Http401UnauthorizedEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
+    }
+}
