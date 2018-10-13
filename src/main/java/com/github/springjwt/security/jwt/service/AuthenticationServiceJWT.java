3edb7d85e48a864c31c82481cc3029878391d176
mariusz <mariusz.maciula@gmail.com>
2018-10-13 13:31:51 +0200

    Created token manager

--- src/main/java/com/github/springjwt/security/jwt/service/AuthenticationServiceJWT.java
+++ src/main/java/com/github/springjwt/security/jwt/service/AuthenticationServiceJWT.java
@@ -1,6 +1,7 @@
 package com.github.springjwt.security.jwt.service;
 
 import com.github.springjwt.security.PrincipalUser;
+import com.github.springjwt.security.jwt.TokenManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
