fd28bc0f41b9b28b28d77988cae4f8cd31b2b335
mariusz <mariusz.maciula@gmail.com>
2018-10-17 12:48:22 +0200

    Connection to the database

--- src/main/java/com/github/springjwt/Application.java
+++ src/main/java/com/github/springjwt/Application.java
@@ -1,7 +1,31 @@
 package com.github.springjwt;
 
-public class Application {
-    public static void main(String[] args){
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.github.springjwt"})
+public class Application extends SpringBootServletInitializer {
+    private static Log logger = LogFactory.getLog(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication application = new SpringApplication(Application.class);
+        ConfigurableApplicationContext context = application.run(args);
+
+        logger.info("Activated Spring Profiles: " + Arrays.toString(context.getEnvironment().getActiveProfiles()));
+        logger.info("Connected to database: " + context.getEnvironment().getProperty("spring.datasource.url"));
     }
 }
