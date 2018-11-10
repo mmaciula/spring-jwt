45d569b4dbf5f854dc866dd6cc4a24732315b06c
mariusz <mariusz.maciula@gmail.com>
2018-11-10 22:28:00 +0100

    Replace token authentication filter with new class (unfinished)

--- src/main/java/com/github/springjwt/security/jwt/filter/AuthorizationTokenFilter.java
+++ src/main/java/com/github/springjwt/security/jwt/filter/AuthorizationTokenFilter.java
@@ -1,6 +1,41 @@
 package com.github.springjwt.security.jwt.filter;
 
-// TODO Authorization Token Filter class
+import com.github.springjwt.security.jwt.service.DaoUserDetailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-public class AuthorizationTokenFilter {
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthorizationTokenFilter extends OncePerRequestFilter {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final DaoUserDetailService userDetailService;
+    private final String tokenHeader;
+
+    public AuthorizationTokenFilter(DaoUserDetailService userDetailService, String tokenHeader) {
+        this.userDetailService = userDetailService;
+        this.tokenHeader = tokenHeader;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        logger.debug("Authentication for ", request.getRequestURL());
+
+        final String requestHeader = request.getHeader(tokenHeader);
+
+        String username = null;
+        String authToken = null;
+
+        if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
+            authToken = requestHeader.substring(7);
+            // TODO get username from token (create TokenUtils class)
+        }
+    }
 }
