dece944b127f0a9dfa25d6676fb8ead1ed411fb9
mariusz <mariusz.maciula@gmail.com>
2018-11-11 00:01:45 +0100

    Change directory for JwtUser

--- src/main/java/com/github/springjwt/security/jwt/JwtUser.java
+++ src/main/java/com/github/springjwt/security/jwt/JwtUser.java
@@ -1,4 +1,4 @@
-package com.github.springjwt.security;
+package com.github.springjwt.security.jwt;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
