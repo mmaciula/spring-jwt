2d125f908ff388dca816985a3a2399c484cfd822
mariusz <mariusz.maciula@gmail.com>
2018-11-15 12:22:54 +0100

    Configuration web security

--- src/main/java/com/github/springjwt/Application.java
+++ src/main/java/com/github/springjwt/Application.java
@@ -1,39 +1,12 @@
 package com.github.springjwt;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Arrays;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.github.springjwt"})
-public class Application extends SpringBootServletInitializer {
-    private static Log logger = LogFactory.getLog(Application.class);
+public class Application {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
-    }
-
-    public static void main(String[] args) throws Throwable {
-        SpringApplication application = new SpringApplication(Application.class);
-        ConfigurableApplicationContext context = application.run(args);
-
-        logger.info("Activated Spring Profiles: " + Arrays.toString(context.getEnvironment().getActiveProfiles()));
-        logger.info("Connected to database: " + context.getEnvironment().getProperty("spring.datasource.url"));
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/github/springjwt/config/ConfigurationWebSecurity.java
+++ src/main/java/com/github/springjwt/config/ConfigurationWebSecurity.java
@@ -1,21 +1,21 @@
 package com.github.springjwt.config;
 
-import com.github.springjwt.security.jwt.filter.TokenAuthenticationFilter;
+import com.github.springjwt.security.jwt.filter.AuthorizationTokenFilter;
 import com.github.springjwt.security.jwt.service.DaoUserDetailService;
 import com.github.springjwt.security.spring.Http401UnauthorizedEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -25,47 +25,49 @@ public class ConfigurationWebSecurity extends WebSecurityConfigurerAdapter {
     @Autowired
     private Http401UnauthorizedEntryPoint http401UnauthorizedEntryPoint;
     @Autowired
-    private DaoUserDetailService daoUserDetailService;
-
-    // TODO Authorization token
+    private DaoUserDetailService userDetailService;
+    @Autowired
+    private AuthorizationTokenFilter authTokenFilter;
+    @Value("${jwt.header}")
+    private String header;
+    @Value("${jwt.authentication.path}")
+    private String authPath;
 
     @Autowired
-    @Lazy
-    private TokenAuthenticationFilter tokenAuthenticationFilter;
-
-    @Bean
-    public DaoAuthenticationProvider customAuthenticationProvider(){
-
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-
-        return authenticationProvider;
-    }
-
-    @Bean
-    @Override
-    @Qualifier("customAuthenticationManager")
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(customAuthenticationProvider());
+    public void configureGlobal(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        authManagerBuilder
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(http401UnauthorizedEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated();
 
-        http.httpBasic()
-                .authenticationEntryPoint(http401UnauthorizedEntryPoint)
-                .and()
-                    .anonymous()
-                .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                    .csrf().disable();
+        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .headers()
+                .frameOptions().sameOrigin()
+                .cacheControl();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
